85dca8e5e6029f07b83fed6764254f68426e25ed
JulesBoissiere <80783874+JulesBoissiere@users.noreply.github.com>
2021-05-17 13:24:34 -0400

    Update EmotionPreset.cs

--- Assets/03_Scripts/EmotionPreset.cs
+++ Assets/03_Scripts/EmotionPreset.cs
@@ -8,7 +8,9 @@ public class EmotionPreset
         JOY,
         CALM,
         SAD,
-        ANGER
+        ANGER,
+        OTHER1,
+        OTHER2
     };
 
     public Vector3 Color1;
@@ -22,6 +24,9 @@ public class EmotionPreset
     public float VeloDmp;
     public Vector3 NoiseStr;
     public float NoiseFrq;
+    public float NoiseOctaveCount;
+    public float NoiseOctaveMultiplier;
+    public float NoiseOctaveScale;
     public float NoiseSpd;
     public float NoisePos;
     public float NoiseRot;
@@ -39,12 +44,15 @@ public class EmotionPreset
     {
         EmotionPreset res = new EmotionPreset();
 
-        EmotionPreset[] emotions = new EmotionPreset[4];
+        EmotionPreset[] emotions = new EmotionPreset[6];
 
-        emotions[0] = getCalm();
-        emotions[1] = getJoy();
-        emotions[2] = getSad();
-        emotions[3] = getAnger();
+        emotions[0] = getCue7();
+        emotions[1] = getCue13();
+        emotions[2] = getCue20();
+        emotions[3] = getCueB22();
+        emotions[4] = getCue32();
+        emotions[5] = getCue41();
+      
 
         res = interpPresetFromArray(emotions, coords);
 
@@ -102,121 +110,193 @@ public class EmotionPreset
         switch (emo)
         {
             case EmoPreset.JOY:
-                return getJoy();
+                return getCue13();
             case EmoPreset.CALM:
-                return getCalm();
+                return getCue7();
             case EmoPreset.SAD:
-                return getSad();
+                return getCue20();
             case EmoPreset.ANGER:
-                return getAnger();
-                
+                return getCueB22();
+            case EmoPreset.OTHER1:
+                return getCue32();
+            case EmoPreset.OTHER2:
+                return getCue41();
+               
             default:
-                return getCalm();
+                return getCue7();
         }
     }
 
     //from table
 
     // JOY is 1, 1
-    public static EmotionPreset getJoy()
+
+    public static EmotionPreset getCue7()
     {
         EmotionPreset preset = new EmotionPreset();
-        preset.ShapePos = new Vector3(0, 0, 850);
-        preset.Color1 = new Vector3(1, 1, 0);
-        preset.Color2 = new Vector3(1.15f, 0.3f, 0);
-        preset.VeloLin = new Vector3(0, 0, 0);
-        preset.VeloOrb = new Vector3(0.98f, 0.87f, 0);
-        preset.VeloRad = -30;
-        preset.VeloSpd = 1;
-        preset.VeloLimSpd = 0;
-        preset.VeloDmp = 0;
-        preset.NoiseStr = new Vector3(10, 0, 53);
-        preset.NoiseFrq = 0.0047f;
-        preset.NoiseSpd = 0;
-        preset.NoisePos = 20;
-        preset.NoiseRot = 1;
-        preset.NoiseScl = 0.5f;
-        preset.SizeLif = 0.8f;
-
-        preset.EmoCoords = new Vector2(1,1);
-
-        return preset;
-    }
-
-    public static EmotionPreset getCalm()
-    {
-        EmotionPreset preset = new EmotionPreset();
-        preset.ShapePos = new Vector3(0, 0, 1000);
-        preset.Color1 = new Vector3(0.2f, 1f, 0.6f);
-        preset.Color2 = new Vector3(0, 0.3f, 0.15f);
-        preset.VeloLin = new Vector3(-0.7f, 0.758f, -0.758f);
+        preset.ShapePos = new Vector3(0, 0, 0);
+        preset.Color1 = new Vector3(2.87f, 0.8f, 0.5f);
+        preset.Color2 = new Vector3(1.27f, -1f, -2.42f);
+        preset.VeloLin = new Vector3(-0.5f, 0.58f, -0.58f);
         preset.VeloOrb = new Vector3(0, 0, 0);
         preset.VeloRad = 0;
         preset.VeloSpd = 0.33f;
         preset.VeloLimSpd = 0;
         preset.VeloDmp = 0;
-        preset.NoiseStr = new Vector3(13, 3, -6);
-        preset.NoiseFrq = 0.00151f;
-        preset.NoiseSpd = 0.68f;
-        preset.NoisePos = -9.1f;
-        preset.NoiseRot = 0.3f;
-        preset.NoiseScl = 0.8f;
-        preset.SizeLif = 0.6f;
+        preset.NoiseStr = new Vector3(13, -6, 0);
+        preset.NoiseFrq = 0.001f;
+        preset.NoiseOctaveCount = 2f;
+        preset.NoiseOctaveMultiplier = 1f;
+        preset.NoiseOctaveScale = .5f;
+        preset.NoiseSpd = 20f;
+        preset.NoisePos = -10f;
+        preset.NoiseRot = 10f;
+        preset.NoiseScl = 0f;
+        preset.SizeLif = 2f;
+
+        preset.EmoCoords = new Vector2(1, 1);
+
+        return preset;
+    }
+    public static EmotionPreset getCue13()
+    {
+        EmotionPreset preset = new EmotionPreset();
+        preset.ShapePos = new Vector3(0, 0, 0);
+        preset.Color1 = new Vector3(2.38f, 0.32f, 0);
+        preset.Color2 = new Vector3(1f, 0, 0);
+        preset.VeloLin = new Vector3(0, 0, 0);
+        preset.VeloOrb = new Vector3(0.1f, 0, 0);
+        preset.VeloRad = 0;
+        preset.VeloSpd = 1f;
+        preset.VeloLimSpd = 0;
+        preset.VeloDmp = 0;
+        preset.NoiseStr = new Vector3(9, 0, 0);
+        preset.NoiseFrq = 0.0024f;
+        preset.NoiseOctaveCount = 1f;
+        preset.NoiseOctaveMultiplier = 0;
+        preset.NoiseOctaveScale = 0;
+        preset.NoiseSpd = 307f;
+        preset.NoisePos = -15f;
+        preset.NoiseRot = 0;
+        preset.NoiseScl = 0;
+        preset.SizeLif = 2f;
+
+        preset.EmoCoords = new Vector2(1, 0);
+
+        return preset;
+    }
+    
+
+  
+
+    public static EmotionPreset getCue20()
+    {
+        EmotionPreset preset = new EmotionPreset();
+        preset.ShapePos = new Vector3(0, 0, 0);
+        preset.Color1 = new Vector3(1f, 0f, 0);
+        preset.Color2 = new Vector3(1.15f, 0.05f, 0);
+        preset.VeloLin = new Vector3(0, 0, 10f);
+        preset.VeloOrb = new Vector3(0, 0, 0);
+        preset.VeloRad = 0;
+        preset.VeloSpd = 2f;
+        preset.VeloLimSpd = 0;
+        preset.VeloDmp = 0;
+        preset.NoiseStr = new Vector3(0, 0, 2);
+        preset.NoiseFrq = 0.009f;
+        preset.NoiseOctaveCount = 1f;
+        preset.NoiseOctaveMultiplier = 0f;
+        preset.NoiseOctaveScale = 1f;
+        preset.NoiseSpd = 60f;
+        preset.NoisePos = -9f;
+        preset.NoiseRot = 0;
+        preset.NoiseScl = 0;
+        preset.SizeLif = 3f;
 
         preset.EmoCoords = new Vector2(1, -1);
 
         return preset;
     }
 
-    public static EmotionPreset getSad()
+    public static EmotionPreset getCueB22()
     {
         EmotionPreset preset = new EmotionPreset();
-        preset.ShapePos = new Vector3(0, 0, 850);
-        preset.Color1 = new Vector3(0.1f, 0.11f, 0.5f);
-        preset.Color2 = new Vector3(0, 0.12f, 0.2f);
+        preset.ShapePos = new Vector3(0, 0, 0);
+        preset.Color1 = new Vector3(0.05f, 0.11f, 0.5f);
+        preset.Color2 = new Vector3(0, 0.1f, 1.84f);
         preset.VeloLin = new Vector3(3, 50, -50);
-        preset.VeloOrb = new Vector3(0.8f, 1.09f, 0.42f);
+        preset.VeloOrb = new Vector3(0.8f, 0.09f, 0f);
         preset.VeloRad = -30;
         preset.VeloSpd = 0.1f;
         preset.VeloLimSpd = 0;
         preset.VeloDmp = 0;
         preset.NoiseStr = new Vector3(-60, 0, 0);
         preset.NoiseFrq = 0.003f;
-        preset.NoiseSpd = 10;
-        preset.NoisePos = 3;
+        preset.NoiseOctaveCount = 2f;
+        preset.NoiseOctaveMultiplier = 1f;
+        preset.NoiseOctaveScale = .2f;
+        preset.NoiseSpd = 100f;
+        preset.NoisePos = 4;
         preset.NoiseRot = 0;
-        preset.NoiseScl = 0.5f;
-        preset.SizeLif = 0.6f;
+        preset.NoiseScl = 0f;
+        preset.SizeLif = 3f;
 
         preset.EmoCoords = new Vector2(-1, -1);
 
         return preset;
     }
-
-    public static EmotionPreset getAnger()
+    public static EmotionPreset getCue32()
     {
         EmotionPreset preset = new EmotionPreset();
-        preset.ShapePos = new Vector3(0, 0, 800);
-        preset.Color1 = new Vector3(0.2f, -0.42f, 0f);
-        preset.Color2 = new Vector3(1f, 0.04f, 0f);
-        preset.VeloLin = new Vector3(0.03f, 0, -50f);
+        preset.ShapePos = new Vector3(0, 0, 0);
+        preset.Color1 = new Vector3(0.2f, -0.3f, 0.01f);
+        preset.Color2 = new Vector3(0.97f, 0.04f, 0.01f);
+        preset.VeloLin = new Vector3(0.03f, 0, -10f);
         preset.VeloOrb = new Vector3(0, 0, 0.15f);
-        preset.VeloRad = -30;
-        preset.VeloSpd = 3.73f;
+        preset.VeloRad = 5;
+        preset.VeloSpd = 4f;
         preset.VeloLimSpd = 0;
         preset.VeloDmp = 0;
-        preset.NoiseStr = new Vector3(29f, 32f, -16f);
-        preset.NoiseFrq = 0.005f;
-        preset.NoiseSpd = 5;
+        preset.NoiseStr = new Vector3(0, 0, 0.9f);
+        preset.NoiseFrq = 0.004f;
+        preset.NoiseOctaveCount = 1;
+        preset.NoiseOctaveMultiplier = 0;
+        preset.NoiseOctaveScale = 0.5f;
+        preset.NoiseSpd = 80;
         preset.NoisePos = 3.6f;
         preset.NoiseRot = 0.5f;
-        preset.NoiseScl = 0.433f;
-        preset.SizeLif = 0.48f;
+        preset.NoiseScl = 0;
+        preset.SizeLif = 2f;
 
-        preset.EmoCoords = new Vector2(-1, 1);
+        preset.EmoCoords = new Vector2(-1, 0);
 
         return preset;
     }
 
+    public static EmotionPreset getCue41()
+       {
+           EmotionPreset preset = new EmotionPreset();
+           preset.ShapePos = new Vector3(0, 0, 0);
+           preset.Color1 = new Vector3(3.5f, 0, 0);
+           preset.Color2 = new Vector3(1, 0.36f, 0.3f);
+           preset.VeloLin = new Vector3(0, 0, 0);
+           preset.VeloOrb = new Vector3(0, 0, .3f);
+           preset.VeloRad = -30f;
+           preset.VeloSpd = 3;
+           preset.VeloLimSpd = 0;
+           preset.VeloDmp = 0;
+           preset.NoiseStr = new Vector3(0, -2, 0);
+           preset.NoiseFrq = 0.002f;
+           preset.NoiseOctaveCount = 2f;
+           preset.NoiseOctaveMultiplier = 4f;
+           preset.NoiseOctaveScale = 0.2f;
+           preset.NoiseSpd = 50;
+           preset.NoisePos = 5;
+           preset.NoiseRot = 1;
+           preset.NoiseScl = 0;
+           preset.SizeLif = 3f;
 
+           preset.EmoCoords = new Vector2(-1, 1);
+
+           return preset;
+       }
 }
